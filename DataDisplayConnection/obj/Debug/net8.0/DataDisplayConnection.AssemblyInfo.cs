c2f8a869bab9bb77f4d70081662006984736befa
abdo74174 <g1g2g3g4g5m1m2m3m4m5@gmail.com>
2026-01-07 07:10:04 +0200

    feat: Create DataDisplayConnection project with DocumentFormat.OpenXml and UglyToad.PdfPig dependencies.

--- DataDisplayConnection/obj/Debug/net8.0/DataDisplayConnection.AssemblyInfo.cs
+++ DataDisplayConnection/obj/Debug/net8.0/DataDisplayConnection.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataDisplayConnection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e4f554efd836eaa9a8830bfe12350e48bbdd09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+099f7c365dfc2b234291f1d4da221d271046ba1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataDisplayConnection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataDisplayConnection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
